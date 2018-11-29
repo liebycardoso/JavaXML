0b9f31654a6e21e899c935b6a548458b1981b5f3
Lieby Cardoso <liebycardoso@gmail.com>
2018-11-29 15:56:38 -0500

    error message

--- XMLHandler.java
+++ XMLHandler.java
@@ -1,8 +1,12 @@
 
+import java.io.File;
+
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
@@ -13,37 +17,51 @@ public class XMLHandler {
 	
 	//ORDER_NUMBER,DATE,TOTAL_VALUE,TOTAL_ITEMS,TOTAL_DISCOUNT,CUSTOMER_NUMBER,
 	// CUSTOMER_FNAME,CUSTOMER_LNAME,ADDRESS,CITY,PROVINCE,COUNTRY,ZIPCODE,
-	String[] params;
-	String order, customer;
+	Document doc;
 	
 	
-	
-	public XMLHandler(String[] params) throws Exception{
-		this.order = params[0];
-		this.customer = params[5];
+	public XMLHandler(Document doc) throws Exception{
 		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
 		DocumentBuilder docBuilder = dbf.newDocumentBuilder();		
-		Document doc = docBuilder.newDocument();
-	}
-
-	public String getOrder() {
-		return order;
-	}
-
-	public void setOrder(String order) {
-		this.order = order;
-	}
-
-	public String getCustomer() {
-		return customer;
-	}
-
-	public void setCustomer(String customer) {
-		this.customer = customer;
+		this.doc = docBuilder.newDocument();
 	}
 	
-	public void createElementRoot(){
+/*	
+	public Document createXMLDoc() throws ParserConfigurationException{
+		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+		DocumentBuilder docBuilder = dbf.newDocumentBuilder();		
+		this.doc = docBuilder.newDocument();
+		return this.doc;
+	}
+*/
+	public void addElementRoot(String rootName)throws Exception{
+		try{
+		Element rootElement = doc.createElement(rootName);
+		doc.appendChild(rootElement);
+		} catch (Exception e){
+			throw new Exception("The method createElementRoot failed", e); 
+		}
 		
+	}
+	
+	
+	
+	public void streamToXML(Document doc, String fileName) throws TransformerException{
+		TransformerFactory transformerFactory = TransformerFactory.newInstance();
+		Transformer transformer;
+		
+		try {
+			transformer = transformerFactory.newTransformer();
+			DOMSource source = new DOMSource(doc);
+			
+			StreamResult streamResult = new StreamResult(new File(fileName));
+			transformer.transform(source, streamResult);
+
+		} catch (TransformerConfigurationException e) {
+			// TODO Auto-generated catch block
+			throw new TransformerConfigurationException("The method streamToXML failed", e);
+			
+		}
 	}
 	
 
